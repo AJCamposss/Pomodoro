4d183ecae735a98113f07fd875a48fa60caa119e
AJCamposss <adrian.campos869@gmail.com>
2023-12-08 21:56:20 -0700

    Started my js file and added script to my index.html

---
+++ scripts/timer.js
@@ -0,0 +1,8 @@
+let pomodoro = document.getElementById("pomodoroTimer");
+let short = document.getElementById("shortTimer");
+let long = document.getElementById("longTimer");
+let currentTimer = null;
+
+function showDefaultTimer () {
+    
+}

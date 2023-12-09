05da9c749ada0f8b36f5e69762be3dc7a9c8879e
AJCamposss <adrian.campos869@gmail.com>
2023-12-08 22:08:08 -0700

    Made it so it will show up my timer depending on which is selected

--- scripts/timer.js
+++ scripts/timer.js
@@ -4,5 +4,27 @@ let long = document.getElementById("longTimer");
 let currentTimer = null;
 
 function showDefaultTimer () {
-    
+    pomodoro.style.display="block";
+    short.style.display ="none";
+    long.style.display = "none";
 }
+
+showDefaultTimer();
+
+document.getElementById("pomodoro").addEventListener("click", function() {
+    hideAll();
+    pomodoro.style.display = "block"
+    currentTimer = pomodoro
+});
+
+document.getElementById("shortBreak").addEventListener("click", function() {
+    hideAll();
+    short.style.display = "block"
+    currentTimer = short
+});
+
+document.getElementById("long-break").addEventListener("click", function () {
+    hideAll();
+    long.style.display = "block"
+    currentTimer = long
+});
